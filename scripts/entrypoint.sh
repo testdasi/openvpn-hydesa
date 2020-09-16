6a0ec44ba567de3fc89e578bc0fe28ba7ab96fc1
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 22:25:48 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -47,15 +47,18 @@ echo "[info] Run tinyproxy in background with no log on port $TINYPROXY_PORT"
 tinyproxy -c /config/tinyproxy/tinyproxy.conf
 
 ### sabnzbdplus
+echo ''
 echo "[info] Run sabnzbdplus in background on HTTP port $SAB_PORT_A and HTTPS port $SAB_PORT_B"
 sabnzbdplus --daemon --config-file /config/sabnzbdplus/sabnzbdplus.ini --pidfile /config/sabnzbdplus/sabnzbd.pid
 
-### deluge-web
-echo "[info] Run deluge-web in background on HTTP port $DELUGE_PORT"
+### deluge
+echo ''
+echo "[info] Run deluge in background on HTTP port $DELUGE_PORT"
 deluged --quiet --port=58846 --config=/config/deluge-web
 deluge-web --fork --quiet --config=/config/deluge-web
 
 ### nzbhydra2
+echo ''
 echo "[info] Run nzbhydra2 in background on port $HYDRA_PORT"
 /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /config/nzbhydra2 --pidfile /config/nzbhydra2/nzbhydra2.pid
 
