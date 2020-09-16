da71500501fd6d02bdeeba10898120c5562f6d79
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 12:30:00 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,54 @@
 #!/bin/bash
 
-### sabnzbdplus config
-echo '[info] Creating sabnzbdplus config if not exist'
-mkdir -p /config/sabnzbdplus \
-    && cp -n /temp/sabnzbdplus.ini /config/sabnzbdplus/
+### Set various variable values ###
+echo ''
+echo '[info] Setting variables'
+source /set_variables.sh
+echo '[info] All variables set'
+
+### Fixing config files ###
+echo ''
+echo '[info] Fixing configs'
+source /fix_config.sh
+echo '[info] All configs fixed'
+
+### Stubby DNS-over-TLS ###
+#echo ''
+#echo "[info] Run stubby in background on port $DNS_PORT"
+#stubby -g -C /etc/stubby/stubby.yml
+#ipnaked=$(dig +short myip.opendns.com @208.67.222.222)
+#echo "[warn] Your ISP public IP is $ipnaked"
+
+### nftables ###
+#echo ''
+#echo '[info] Set up nftables rules'
+#source /nftables.sh
+#echo '[info] All rules created'
+
+### Quick block test ####
+#echo ''
+#ipttest=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
+#echo "[info] Quick block test. Expected result is time out. Actual result is $ipttest"
+
+### OpenVPN ###
+#echo ''
+#echo "[info] Setting up OpenVPN tunnel"
+#source /openvpn.sh
+#echo '[info] Done'
+
+### Dante SOCKS proxy to VPN ###
+#echo ''
+#echo "[info] Run danted in background on port $DANTE_PORT"
+#danted -D -f /etc/danted.conf
+
+### Tinyproxy HTTP proxy to VPN ###
+#echo ''
+#echo "[info] Run tinyproxy in background with no log on port $TINYPROXY_PORT"
+#tinyproxy -c /etc/tinyproxy/tinyproxy.conf
+
+### sabnzbdplus
+echo '[info] Run sabnzbdplus in background'
+sabnzbdplus --config-file /config/sabnzbdplus/sabnzbdplus.ini
 
 ### Infinite loop to stop docker from stopping ###
 while true
