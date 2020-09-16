6c5a8643624ab4617903b85068600e3c7b5040e5
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 19:59:10 +0100

    Update fix_config.sh

--- scripts/fix_config.sh
+++ scripts/fix_config.sh
@@ -19,10 +19,19 @@ sed -i "s|Port 8080|Port $TINYPROXY_PORT|g" '/config/tinyproxy/tinyproxy.conf'
 sed -i "s|upstream socks5 localhost:1080|upstream socks5 $ETH0_IP:$DANTE_PORT|g" '/config/tinyproxy/tinyproxy.conf'
 echo '[info] tinyproxy fixed.'
 
+mkdir -p /config/nzbhydra2 \
+    && cp -n /temp/nzbhydra.yml /config/nzbhydra2/
+sed -i "s|port: 5076|port: $HYDRA_PORT|g" '/config/nzbhydra2/nzbhydra.yml'
+sed -i "s|127\.0\.0\.1:8080|127\.0\.0\.1:$SAB_PORT_A|g" '/config/nzbhydra2/nzbhydra.yml'
+echo '[info] nzbhydra2 fixed.'
+
 mkdir -p /config/sabnzbdplus \
     && cp -n /temp/sabnzbdplus.ini /config/sabnzbdplus/ \
     && mkdir -p /data/sabnzbdplus/watch \
     && mkdir -p /data/sabnzbdplus/incomplete \
     && mkdir -p /data/sabnzbdplus/complete \
     && mkdir -p /data/sabnzbdplus/script
+sed -i "s|port = 8080|port = $SAB_PORT_A|g" '/config/sabnzbdplus/sabnzbdplus.ini'
+sed -i "s|https_port = 8090|https_port = $SAB_PORT_B|g" '/config/sabnzbdplus/sabnzbdplus.ini'
 echo '[info] sabnzbdplus fixed.'
+
