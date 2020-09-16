8520f75c2fabb2530757ee3b1d7676b8d117897e
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-16 12:30:13 +0100

    Create fix_config.sh

---
+++ scripts/fix_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sed -i "s|  - 0\.0\.0\.0\@53|  - 0\.0\.0\.0\@$DNS_PORT|g" '/etc/stubby/stubby.yml'
+#echo '[info] stubby fixed.'
+
+#sed -i "s|internal: eth0 port=1080|internal: eth0 port=$DANTE_PORT|g" '/etc/danted.conf'
+#echo '[info] danted fixed.'
+
+#sed -i "s|Port 8080|Port $TINYPROXY_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
+#sed -i "s|upstream socks5 localhost:1080|upstream socks5 $ETH0_IP:$DANTE_PORT|g" '/etc/tinyproxy/tinyproxy.conf'
+#echo '[info] tinyproxy fixed.'
+
+mkdir -p /config/sabnzbdplus \
+    && cp -n /temp/sabnzbdplus.ini /config/sabnzbdplus/
+echo '[info] sabnzbdplus fixed.'
