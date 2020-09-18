57a577de63b45ee0c4a215f1897c53b89057446c
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 17:04:27 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,5 +10,17 @@ apt-get -y autoremove \
     && apt-get -y clean \
     && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
 
+# install static files
+mkdir -p /temp \
+    && cd /temp \
+    && curl -L "https://github.com/testdasi/static/archive/master.zip" -o /temp/static.zip \
+    && unzip /temp/static.zip \
+    && rm -f /temp/static.zip \
+    && mv /temp/static-master /static
+
+# overwrite static with repo-specific stuff
+cp -f /temp/* /static/config/ \
+    && rm -rf /temp
+
 # chmod scripts
 chmod +x /*.sh
