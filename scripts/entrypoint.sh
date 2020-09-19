df2474ddfcb8202fbc35edc72434f63088b2c7bc
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-19 18:46:13 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,11 +25,6 @@ echo '[info] Set up nftables rules'
 source /nftables.sh
 echo '[info] All rules created'
 
-### Quick block test ####
-echo ''
-ipttest=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
-echo "[info] Quick block test. Expected result is time out. Actual result is $ipttest"
-
 ### OpenVPN ###
 echo ''
 echo "[info] Setting up OpenVPN tunnel"
@@ -68,27 +63,103 @@ echo "[info] Run GUI launcher in background at $LAUNCHER_IP:$LAUNCHER_PORT"
 screen -d -m -fa -S launcher /app/launcher/launcher-python3.sh
 
 ### Infinite loop to stop docker from stopping ###
-sleep 10s
+sleep_time=10
+crashed=0
 while true
 do
     echo ''
+    echo "[info] Wait $sleep_time seconds before next healthcheck..."
+    sleep $sleep_time
+    
     iphiden=$(dig +short myip.opendns.com @208.67.222.222)
     echo "[info] Your VPN public IP is $iphiden"
-    pidlist=$(pidof openvpn)
-    echo "[info] OpenVPN PID: $pidlist"
+    
     pidlist=$(pidof stubby)
-    echo "[info] stubby PID: $pidlist"
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] stubby crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        stubby -g -C /root/stubby/stubby.yml
+    else
+        echo "[info] stubby PID: $pidlist"
+    fi
+    
+    pidlist=$(pidof openvpn)
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] openvpn crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        source /static/scripts/openvpn.sh
+    else
+        echo "[info] openvpn PID: $pidlist"
+    fi
+    
     pidlist=$(pidof danted)
-    echo "[info] danted PID: $pidlist"
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] danted crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        danted -D -f /root/dante/danted.conf
+    else
+        echo "[info] danted PID: $pidlist"
+    fi
+    
     pidlist=$(pidof tinyproxy)
-    echo "[info] tinyproxy PID: $pidlist"
-    pidlist=$(cat /config/sabnzbdplus/sabnzbd.pid)
-    echo "[info] sabnzbdplus PID: $pidlist"
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] tinyproxy crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        tinyproxy -c /root/tinyproxy/tinyproxy.conf
+    else
+        echo "[info] tinyproxy PID: $pidlist"
+    fi
+    
+    #pidlist=$(cat /root/sabnzbdplus/sabnzbd.pid)
+    pidlist=$(pidof python2)
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] sabnzbdplus crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        sabnzbdplus --daemon --config-file /root/sabnzbdplus/sabnzbdplus.ini --pidfile /root/sabnzbdplus/sabnzbd.pid
+    else
+        echo "[info] sabnzbdplus PID: $pidlist"
+    fi
+    
     pidlist=$(cat /config/deluge-web/deluged.pid | sed 's/;58846//g')
     echo "[info] deluge PID: $pidlist"
-    pidlist=$(cat /config/nzbhydra2/nzbhydra2.pid)
-    echo "[info] nzbhydra2 PID: $pidlist"
+    
+    #pidlist=$(cat /root/nzbhydra2/nzbhydra2.pid)
+    pidlist=$(pidof nzbhydra2)
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] nzbhydra2 crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /root/nzbhydra2 --pidfile /root/nzbhydra2/nzbhydra2.pid
+    else
+        echo "[info] nzbhydra2 PID: $pidlist"
+    fi
+    
     pidlist=$(pidof python3)
-    echo "[info] GUI launcher PID: $pidlist"
-    sleep 3600s
+    if [ -z "$pidlist" ]
+    then
+        echo '[warn] WebUI launcher crashed, restarting'
+        crashed=$(( $crashed + 1 ))
+        screen -d -m -fa -S launcher /app/launcher/launcher-python3.sh
+    else
+        echo "[info] WebUI launcher PID: $pidlist"
+    fi
+    
+    # reset wait time if something crashed, otherwise double the wait time till next healthcheck
+    if (( $crashed > 0 ))
+    then
+        sleep_time=$(( $crashed * 10 ))
+        crashed=0
+    else
+        sleep_time=$(( $sleep_time * 2 ))
+        # restrict wait time to within 3600s i.e. 1hr
+        if (( $sleep_time > 3600 ))
+        then
+            sleep_time=3600
+        fi
+    fi
 done
