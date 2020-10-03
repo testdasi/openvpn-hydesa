64af3636a23d906360b5a3e3342a19a2793cf0c8
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 17:43:07 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -66,106 +66,14 @@ then
     echo "[info] Run GUI launcher in background at $LAUNCHER_IP:$LAUNCHER_PORT"
     start-stop-daemon --start --background --name launcher --chdir /app/launcher --exec /app/launcher/launcher-python3.sh
 
-    ### Infinite loop to stop docker from stopping ###
-    sleep_time=10
-    crashed=0
+    ### Periodically checking IP ###
+    sleep_time=3600
+    echo ''
     while true
     do
-        echo ''
-        echo "[info] Wait $sleep_time seconds before next healthcheck..."
-        sleep $sleep_time
-        
         iphiden=$(dig +short myip.opendns.com @208.67.222.222)
         echo "[info] Your VPN public IP is $iphiden"
-        
-        pidlist=$(pidof stubby)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] stubby crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            stubby -g -C /root/stubby/stubby.yml
-        else
-            echo "[info] stubby PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof openvpn)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] openvpn crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            source /static/scripts/openvpn.sh
-        else
-            echo "[info] openvpn PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof danted)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] danted crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            danted -D -f /root/dante/danted.conf
-        else
-            echo "[info] danted PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof tinyproxy)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] tinyproxy crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            tinyproxy -c /root/tinyproxy/tinyproxy.conf
-        else
-            echo "[info] tinyproxy PID: $pidlist"
-        fi
-        
-        #pidlist=$(cat /root/sabnzbdplus/sabnzbd.pid)
-        pidlist=$(pidof python2)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] sabnzbdplus crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            sabnzbdplus --daemon --config-file /root/sabnzbdplus/sabnzbdplus.ini --pidfile /root/sabnzbdplus/sabnzbd.pid
-        else
-            echo "[info] sabnzbdplus PID: $pidlist"
-        fi
-        
-        pidlist=$(cat /config/deluge-web/deluged.pid | sed 's/;58846//g')
-        echo "[info] deluge PID: $pidlist"
-        
-        #pidlist=$(cat /root/nzbhydra2/nzbhydra2.pid)
-        pidlist=$(pidof nzbhydra2)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] nzbhydra2 crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /root/nzbhydra2 --pidfile /root/nzbhydra2/nzbhydra2.pid
-        else
-            echo "[info] nzbhydra2 PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof python3)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] WebUI launcher crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            start-stop-daemon --start --background --name launcher --chdir /app/launcher --exec /app/launcher/launcher-python3.sh
-        else
-            echo "[info] WebUI launcher PID: $pidlist"
-        fi
-        
-        # reset wait time if something crashed, otherwise double the wait time till next healthcheck
-        if (( $crashed > 0 ))
-        then
-            sleep_time=$(( $crashed * 10 ))
-            crashed=0
-        else
-            sleep_time=$(( $sleep_time * 2 ))
-            # restrict wait time to within 3600s i.e. 1hr
-            if (( $sleep_time > 3600 ))
-            then
-                sleep_time=3600
-            fi
-        fi
+        sleep $sleep_time
     done
 else
     echo '[CRITICAL] Config file not found, quitting...'
