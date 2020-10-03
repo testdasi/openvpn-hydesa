ead796695bd8e945e8deb1e70bb31490c47af947
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-03 17:42:03 +0100

    Create healthcheck.sh

---
+++ scripts/healthcheck.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+### Autoheal ###
+crashed=0
+
+pidlist=$(pidof stubby)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    stubby -g -C /config/stubby/stubby.yml
+fi
+
+pidlist=$(pidof danted)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    danted -D -f /config/dante/danted.conf
+fi
+
+pidlist=$(pidof tinyproxy)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    tinyproxy -c /config/tinyproxy/tinyproxy.conf
+fi
+
+pidlist=$(pgrep sabnzbdplus)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    sabnzbdplus --daemon --config-file /config/sabnzbdplus/sabnzbdplus.ini --pidfile /config/sabnzbdplus/sabnzbd.pid
+fi
+
+# deluge
+
+pidlist=$(pgrep nzbhydra2)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /config/nzbhydra2 --pidfile /config/nzbhydra2/nzbhydra2.pid
+fi
+
+pidlist=$(pidof python3)
+if [ -z "$pidlist" ]
+then
+    crashed=$(( $crashed + 1 ))
+    start-stop-daemon --start --background --name launcher --chdir /app/launcher --exec /app/launcher/launcher-python3.sh
+fi
+
+### Critical check ###
+pidlist=$(pidof openvpn)
+if [ -z "$pidlist" ]
+then
+    # kill the docker (by killing init script) if openvpn crashed
+    pidentry=$(pgrep entrypoint.sh)
+    kill $pidentry
+    exit 1
+else
+    # return exit code for healthcheck
+    if (( $crashed > 0 ))
+    then
+        exit 1
+    else
+        exit 0
+    fi
+fi
